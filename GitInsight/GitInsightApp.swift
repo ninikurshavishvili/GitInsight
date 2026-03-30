346c8332bc43613f21ddf0c0512b07a6b637fcff
ninikurshavishvili <nino.kurshavishvili.1@btu.edu.ge>
2026-03-30 16:04:09 +0400

    Initial Commit

---
+++ GitInsight/GitInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInsightApp.swift
+//  GitInsight
+//
+//  Created by Nini Kurshavishvili on 30.03.26.
+//
+
+import SwiftUI
+
+@main
+struct GitInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
