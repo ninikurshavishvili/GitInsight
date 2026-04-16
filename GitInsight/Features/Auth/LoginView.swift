3a811dc0c96ebd60c9f7263e5fa18959c0bf7cf3
ninikurshavishvili <nino.kurshavishvili.1@btu.edu.ge>
2026-04-16 23:36:45 +0400

    Move FooterView to seperate File

--- GitInsight/Features/Auth/LoginView.swift
+++ GitInsight/Features/Auth/LoginView.swift
@@ -225,19 +225,6 @@ struct SecurityBadgeRow: View {
     }
 }
 
-// MARK: - Footer
-
-private struct FooterView: View {
-    var body: some View {
-        Text("Built for developers by developers")
-            .font(.system(size: 10, weight: .medium))
-            .textCase(.uppercase)
-            .tracking(1.6)
-            .foregroundStyle(Color.textMuted.opacity(0.35))
-            .frame(maxWidth: .infinity, alignment: .center)
-            .accessibilityLabel("Built for developers by developers")
-    }
-}
 
 
 
